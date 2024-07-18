bec61eae1288b672ddfa079c02b99c479b23e92f
Jordyn <jordynchalmers6976@gmail.com>
2024-07-18 02:05:09 +0100

    Slight Fix
    Changelog:
    
    • Line Removal

--- build.bat
+++ build.bat
@@ -2,13 +2,10 @@
 
 pip install -r requirements.txt
 pyinstaller -w --icon=resources/icon.ico --onefile --name "OSU-Cleaner" src/main.py
-
 del /f /s /q Build
 rmdir /s /q Build
-
 move dist\OSU-Cleaner.exe ..\osu-cleaner-main
 rmdir /s /q dist
 ren OSU-Cleaner.exe osu!cleaner.exe
-
 del OSU-Cleaner.spec
-pause
+
