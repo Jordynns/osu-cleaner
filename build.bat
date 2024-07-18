f4ca5375246ab31d5023826c530e7421f77d96e1
Jordyn <jordynchalmers6976@gmail.com>
2024-07-18 01:26:31 +0100

    Included .exe File Icon
    Changelog:
    
    • Added Flag to Specify .exe File Icon

--- build.bat
+++ build.bat
@@ -1,13 +1,14 @@
 @echo off
 
 pip install -r requirements.txt
-pyinstaller -F -w src/main.py --name "OSU-Cleaner"
+pyinstaller -w --icon=resources/icon.ico --onefile --name "OSU-Cleaner" src/main.py
 
 del /f /s /q Build
 rmdir /s /q Build
 
 move dist\OSU-Cleaner.exe ..\osu-cleaner-main
 rmdir /s /q dist
+ren OSU-Cleaner.exe OSU-Cleaner.exe
 
 del OSU-Cleaner.spec
 pause
